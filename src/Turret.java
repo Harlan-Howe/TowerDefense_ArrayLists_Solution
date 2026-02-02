32c7c3df6f06926de17a820a3a372577e4438ad8
Harlan Howe <harlan.howe@kinkaid.org>
2026-02-02 10:44:40 -0600

    Improved documentation Guns now only point at invaders in range. Color list is now final.

--- src/Turret.java
+++ src/Turret.java
@@ -3,42 +3,53 @@ import java.util.ArrayList;
 
 public class Turret
 {
-    private int myRange;
-    private double myRechargeTime;
-    private int myDamage;
-    private int myType;
-    private World myWorld;
+    private int myRange; // detection radius for invaders (center-to-center)
+    private double myRechargeTime; // minimum time between shots for this turret
+    private int myDamage; // how much damage this turret does
+    private int myType; // my type of turret.
+
+    private World myWorld; // a pointer to the world where this turret will live.
     private double myTimeSinceLastFire;
     private int[] myLoc;
     private double myGunAngle;
 
+    // constants for the class, not the individual turret. (i.e., static variables)
+    // note "final" means we're defining them here, and they may never change.
     private static Stroke thinLine;
     private static Stroke thickLine;
     private static Stroke gunLine;
-    private static Color[] shotColors = {Color.GREEN, Color.BLUE, Color.PINK};
+    private static final Color[] shotColors = {Color.GREEN, Color.BLUE, Color.PINK};
     public static final int[] RANGES = {70, 100, 150};
     public static final double[] RECHARGE_TIMES = {1.0, 2.0, 6.0};
     public static final int[] DAMAGES = {10, 15, 40};
 
     public Turret(int type, World myWorld)
     {
+        // based on "type," select the range, recharge time, and damage done by this turret.
         this.myRange = RANGES[type];
         this.myRechargeTime = RECHARGE_TIMES[type];
         this.myDamage = DAMAGES[type];
         this.myType = type;
+
         this.myWorld = myWorld;
-        myTimeSinceLastFire = 999;
+        // reset recharge to fully charged.
+        resetTimer();
+
+        // start this turret offscreen.
         myLoc = new int[2];
         myLoc[0] = -100;
         myLoc[1] = -100;
+
+        // the gun starts pointing east.
         myGunAngle = 0;
 
+        // these are static variables, so if this is the first turret we created, we need to initialize them once.
         if (null == thickLine)
+        {
             thinLine = new BasicStroke(1);
-        if (null == thickLine)
             thickLine = new BasicStroke(2);
-        if (null == gunLine)
             gunLine = new BasicStroke(4);
+        }
     }
 
     public int[] getMyLoc() {return myLoc;}
@@ -54,10 +65,21 @@ public class Turret
         myLoc[1] = y;
     }
 
+    /**
+     * resets the timer so that this turret is fully charged again.
+     */
     public void resetTimer() {myTimeSinceLastFire = 999;}
 
+    /**
+     * draws this turret, centered at myLoc, depending on what this turret's "myType" is.
+     * @param g - the Graphics object that indicates where on the screen to draw and the tools to do it.
+     *          Note: this usually will be the main panel, but this method also gets called and sent a graphics for
+     *                the images used in the buttons at the top of the screen.
+     */
     public void drawSelf(Graphics g)
     {
+        // Note: we sometimes cast "g" as a "Graphics2D" object, so we can use stroke commands to make the lines thicker.
+        // most of the time in modern programs, g really is an instance of this subclass of Graphics, so it works!
         switch (myType)
         {
             case 0:
@@ -84,10 +106,18 @@ public class Turret
                 for (int i=0; i<3; i++)
                     g.drawLine(x_vals[i], y_vals[i], x_vals[i+3], y_vals[i+3]);
         }
+
+        // draw the circle indicating the state of this gun's charge
         drawRecharge((Graphics2D)g);
+        // draw the gun on this turret to indicate where it is pointing.
         drawGun((Graphics2D)g);
     }
 
+    /**
+     * draws a partial ring around the center of this turret. The color and completeness of the ring indicate whether
+     * it is ready to fire.
+     * @param g - the Graphics object that indicates where on the screen to draw and the tools to do it.
+     */
     private void drawRecharge(Graphics2D g)
     {
         g.setStroke(thickLine);
@@ -105,6 +135,10 @@ public class Turret
         g.setStroke(thinLine);
     }
 
+    /**
+     * draws the gun (the actual turret on this turret)
+     * @param g - the Graphics object that indicates where on the screen to draw and the tools to do it.
+     */
     private void drawGun(Graphics2D g)
     {
         g.setColor(Color.BLACK);
@@ -113,14 +147,23 @@ public class Turret
         g.setStroke(thinLine);
     }
 
+    /**
+     * updates the turret to reflect an animation step. In this case, it just recharges the gun a little bit.
+     * @param deltaT - the time, in seconds, since the last animation step.
+     */
     public void advance(double deltaT)
     {
         myTimeSinceLastFire += deltaT;
     }
 
+    /**
+     * aims the turret at the nearest invader in range. If the gun is fully charged, it shoots the invader.
+     */
     public void targetNearestInvader()
     {
         ArrayList<Invader> invaders = myWorld.getInvaderList();
+
+        // identify which invader is nearest to this gun.
         double nearestDist2 = 999999;
         Invader nearestInv = null;
         for (Invader inv:invaders)
@@ -134,17 +177,20 @@ public class Turret
                 nearestInv = inv;
             }
         }
-        if (nearestInv != null)
+        // if we've identified a nearest invader and it is within range...
+        if (nearestInv != null && (nearestDist2 < Math.pow(myRange,2)))
         {
-            double[] invLoc = nearestInv.getLoc();
 
+            // point the gun at this nearest invader.
+            double[] invLoc = nearestInv.getLoc();
             myGunAngle = Math.atan2(nearestInv.getLoc()[1]-myLoc[1],nearestInv.getLoc()[0]-myLoc[0]);
 
-            if ((nearestDist2 < Math.pow(myRange,2)) && (myTimeSinceLastFire >= myRechargeTime))
+            // if my gun is charged, fire at the invader!
+            if (myTimeSinceLastFire >= myRechargeTime)
             {
                 myWorld.addShot(myLoc, invLoc, shotColors[myType]);
                 myWorld.damageInvader(nearestInv, myDamage);
-                myTimeSinceLastFire = 0;
+                myTimeSinceLastFire = 0; // and the gun now needs to recharge.
             }
         }
     }
